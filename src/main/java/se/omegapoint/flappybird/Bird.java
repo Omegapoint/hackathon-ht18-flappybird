a3a3d17e07a501857dc704b7a2c5e5f9dec86021
Tobias <tobaxe@omegapoint.se>
2018-10-12 19:40:13 +0200

    bird graphics

--- src/main/java/se/omegapoint/flappybird/Bird.java
+++ src/main/java/se/omegapoint/flappybird/Bird.java
@@ -1,12 +1,22 @@
 package se.omegapoint.flappybird;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Bird {
     private final Rectangle hitBox;
+    private Image divingBird;
 
     public Bird(final int xPos, final int yPos, final int width, final int height) {
         hitBox = new Rectangle(xPos, yPos, width, height);
+    }
+
+    private void loadImage() {
+        ImageIcon ii = new ImageIcon("src/main/resources/flying-taco.png");
+        divingBird = ii.getImage();
+
+        hitBox.width = divingBird.getWidth(null);
+        hitBox.height = divingBird.getHeight(null);
     }
 
     public int width() {
